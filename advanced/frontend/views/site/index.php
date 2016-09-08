b7f832a99c3f3693f541152f36e0d5622db6c975
ckh <djzitjkl7m4@gmail.com>
2016-09-08 18:17:33 +1000

    upload advanced version

---
+++ advanced/frontend/views/site/index.php
@@ -0,0 +1,30 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'HI';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome!</h1>
+
+        <p><h2>Start your first course today!</h2></p>
+
+        <p><a class="btn btn-lg btn-success" href="../views/site/index.php?r=site/signup">Sign Up</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+</div>

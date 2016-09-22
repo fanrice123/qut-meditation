7f998b4dca25adad80bc69fdf06e9df267d381b3
Chun Huei <fchunhuei@gmail.com>
2016-09-22 21:58:35 +1000

    deleted redundant files

--- advanced/frontend/controllers/CourseController.php
+++ advanced/frontend/controllers/CourseController.php
@@ -7,19 +7,20 @@ class CourseController extends \yii\web\Controller
     public function actionEnroll($id, $date) {
 
         if (Yii::$app->user->isGuest) {
-            $this->redirect('/site/login');
+            return $this->redirect('/site/login');
         }
 
         $student = new Student();
         $student->studentID = Yii::$app->user->identity->id;
-        $student->courseID = Yii::$id;
+        $student->courseID = $id;
+
         if ($student->save()) {
             Yii::$app->session->setFlash('success', 'You have successfully enrolled the class starting on '.$date);
         }
         else {
             Yii::$app->session->setFlash('warning', 'Sorry, you have already enrolled the class');
         }
-        $this->redirect('/site/course');
+        return $this->redirect('site/course');
 
     }
 
